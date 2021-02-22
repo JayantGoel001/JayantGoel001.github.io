157be8bbe0d6140477b233b80183230707b7502f
username <54479676+CormacKrum@users.noreply.github.com>
2021-02-22 21:01:58 +0530

    Initial Commit

--- js/app.js
+++ js/app.js
@@ -34,3 +34,22 @@ VanillaTilt.init(document.querySelectorAll(".box"), {
     speed: 400
 });
 
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt',(e)=>{
+    e.preventDefault();
+    deferredPrompt = e;
+});
+// window.addEventListener('click',()=>{
+//     deferredPrompt.prompt();
+//     deferredPrompt.userChoice.then((choiceResult)=>{
+//         if (choiceResult.outcome === "accepted"){
+//             console.log("App Installed");
+//         }
+//         deferredPrompt = null;
+//     })
+// })
+window.addEventListener('appinstalled',()=>{
+
+})
+
