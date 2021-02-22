3160cc1302162c45edc7c1227e801cfcbf08edc7
username <54479676+CormacKrum@users.noreply.github.com>
2021-02-23 05:18:00 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -22,7 +22,7 @@
     if('serviceWorker' in navigator) {
         navigator.serviceWorker
             .register('service-worker.js')
-            .then(function() { console.log('Service Worker Registered'); });
+            .then(function() { console.log(''); });
     }
 }(jQuery);
 
