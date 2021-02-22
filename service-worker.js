de56dba7728a11e06ffc1b1cdd26c822cbe525ed
username <54479676+CormacKrum@users.noreply.github.com>
2021-02-23 04:35:01 +0530

    Initial Commit

--- service-worker.js
+++ service-worker.js
@@ -76,7 +76,6 @@ Cache.prototype.addAll||(Cache.prototype.addAll=function(t){function e(t){this.n
 });
 
 
-//# sourceMappingURL=sw-toolbox.map.json
 // *** End of auto-included sw-toolbox code. ***
 
 
