0bdf9a9c54312b20ffede7950a2690773bf2703c
JayantGoel001 <54479676+CormacKrum@users.noreply.github.com>
2021-02-28 01:13:28 +0530

    Initial Commit

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@
 }(jQuery);
 if('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('service-worker.js')
+        .register('./service-worker.js')
         .then(function() { console.log(''); });
 }
 
