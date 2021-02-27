cb563b2660c0a36f28494724596ddaa0c4198147
JayantGoel001 <54479676+CormacKrum@users.noreply.github.com>
2021-02-28 03:22:33 +0530

    Initial Commit

--- service-worker.js
+++ service-worker.js
@@ -33,7 +33,7 @@ const staticAssets=[
     './js/vanilla-tilt.min.js'
 ];
 
-var cacheName = 'cache-2';
+var cacheName = 'cache-3';
 self.addEventListener('install', async ()=>{
     const cache = await caches.open(cacheName);
     await cache.addAll(staticAssets);
