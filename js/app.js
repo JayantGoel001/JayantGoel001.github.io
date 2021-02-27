cf9b825fa15deec3838ae1f0b083bc2f2d15b609
JayantGoel001 <54479676+CormacKrum@users.noreply.github.com>
2021-02-28 02:51:39 +0530

    Initial Commit

--- js/app.js
+++ js/app.js
@@ -20,12 +20,40 @@
         offset: 70
     });
 }(jQuery);
-if('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('./service-worker.js')
-        .then(function() { console.log(''); });
-}
+// if('serviceWorker' in navigator) {
+//     navigator.serviceWorker
+//         .register('./service-worker.js')
+//         .then(function() { console.log(''); });
+// }
+let newWorker;
 
+document.getElementById('reload').addEventListener('click', function(){
+    newWorker.postMessage({ action: 'skipWaiting' });
+});
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js').then(reg => {
+        reg.addEventListener('updatefound', () => {
+            newWorker = reg.installing;
+            newWorker.addEventListener('statechange', () => {
+                switch (newWorker.state) {
+                    case 'installed':
+                        if (navigator.serviceWorker.controller) {
+                            let notification = document.getElementById('notification');
+                            notification.className = 'show';
+                        }
+                        break;
+                }
+            });
+        });
+    });
+}
+let refreshing;
+navigator.serviceWorker.addEventListener('controllerchange', function () {
+    if (refreshing) return;
+    window.location.reload();
+    refreshing = true;
+});
 $(function () {
     $('[data-toggle="tooltip"]').tooltip({ trigger: "hover" })
 })
