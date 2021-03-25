e0aa05d7b0397c679429ad5e875138272c7c37f8
JayantGoel001 <54479676+CormacKrum@users.noreply.github.com>
2021-03-25 12:31:20 +0530

    Initial Commit

--- service-worker.js
+++ service-worker.js
@@ -1,49 +1,9 @@
-const staticAssets=[
-    './',
-    './index.html',
-    './css/styles.min.css',
-    './js/app.min.js',
-    './manifest.json',
-    './service-worker.js',
-    './assets/particles.json',
-    './images/aiicb.webp',
-    './images/aimg.webp',
-    './images/asm.webp',
-    './images/bgimg.webp',
-    './images/data-science.webp',
-    './images/ecs.webp',
-    './images/footer-cloud.svg',
-    './images/gist.webp',
-    './images/HackerRank.webp',
-    './images/hacktoberfest2020.webp',
-    './images/monkey.webp',
-    './images/Profile-pic.webp',
-    './images/favicon.ico',
-    './images/quote-img.webp',
-    './images/sda.webp',
-    './images/us.webp',
-    './images/about-bg.svg',
-    './images/about-bg-sm.svg',
-    './js/bootstrap.bundle.min.js',
-    './js/jquery.easing.min.js',
-    './js/particles.min.js',
-    './js/scrollspy.min.js',
-    './js/dark-light-theme.min.js',
-    './js/typed.init.js',
-    './js/typed.min.js',
-    './js/vanilla-tilt.min.js',
-    './404.html',
-    './css/404.min.css',
-    './js/404.min.js',
-    './images/404.webp'
-];
-
+const staticAssets=['./', './index.html', './css/styles.min.css', './js/app.min.js', './manifest.json', './service-worker.js', './assets/particles.json', './images/aiicb.webp', './images/aimg.webp','./images/asm.webp', './images/bgimg.webp', './images/data-science.webp', './images/ecs.webp', './images/footer-cloud.svg', './images/gist.webp', './images/HackerRank.webp', './images/hacktoberfest2020.webp', './images/monkey.webp', './images/Profile-pic.webp', './images/favicon.ico', './images/quote-img.webp', './images/sda.webp', './images/us.webp', './images/about-bg.svg', './images/about-bg-sm.svg', './js/bootstrap.bundle.min.js', './js/jquery.easing.min.js', './js/particles.min.js', './js/scrollspy.min.js', './js/dark-light-theme.min.js', './js/typed.init.js', './js/typed.min.js', './js/vanilla-tilt.min.js', './404.html', './css/404.min.css', './js/404.min.js', './images/404.webp'];
 var cacheName = 'cache-4';
 self.addEventListener('install', async ()=>{
     const cache = await caches.open(cacheName);
     await cache.addAll(staticAssets);
 });
-
 self.addEventListener('fetch', function(event) {
     event.respondWith(caches.match(event.request).then(function(response) {
         if (response !== undefined) {
@@ -51,7 +11,6 @@ self.addEventListener('fetch', function(event) {
         } else {
             return fetch(event.request).then(function (response) {
                 let responseClone = response.clone();
-
                 caches.open(cacheName).then(function (cache) {
                     cache.put(event.request, responseClone);
                 });
