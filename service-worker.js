4af8efbea981d13682c472ee9b67b185ee1d9e9d
JayantGoel001 <54479676+CormacKrum@users.noreply.github.com>
2021-03-29 13:41:53 +0530

    Initial Commit

--- service-worker.js
+++ service-worker.js
@@ -9,7 +9,7 @@ self.addEventListener('install', function(event) {
 });
 self.addEventListener('fetch', function(event) {
     event.respondWith(caches.match(event.request).then(function(response) {
-        if (response!==undefined) {
+        if (response !==undefined) {
             return response;
         } else {
             var requestClone = event.request.clone()
