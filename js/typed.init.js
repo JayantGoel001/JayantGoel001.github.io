07d64682430d75c97cae2fadce39b4ff53715a9d
JayantGoel001 <54479676+CormacKrum@users.noreply.github.com>
2021-04-09 05:43:36 +0530

    Initial Commit

--- js/typed.init.js
+++ js/typed.init.js
@@ -1,8 +1,1 @@
-$(".element").each(function() {
-    const $this = $(this);
-    $this.typed({
-        strings: $this.attr('data-elements').split(','),
-        typeSpeed: 100,
-        backDelay: 3000
-    });
-});
+$(".element").each(function() {const $this = $(this);$this.typed({strings: $this.attr('data-elements').split(','), typeSpeed: 100, backDelay: 3000});});
