00a3bbb56e1593535f7bd906ec79bdcbb12a1069
JayantGoel001 <jgoel92@gmail.com>
2021-04-23 18:38:58 +0530

    Initial Commit

--- js/app.js
+++ js/app.js
@@ -19,44 +19,29 @@
     $("#navbarCollapse").scrollspy({
         offset: 70
     });
-}(jQuery);
-let newWorker;
-
-document.getElementById('reload').addEventListener('click', function(){
-    newWorker.postMessage({ action: 'skipWaiting' });
-});
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js').then(reg => {
-        reg.addEventListener('updatefound', () => {
-            newWorker = reg.installing;
-            newWorker.addEventListener('statechange', () => {
-                switch (newWorker.state) {
-                    case 'installed':
-                        if (navigator.serviceWorker.controller) {
-                            let notification = document.getElementById('notification');
-                            notification.className = 'show';
-                        }
-                        break;
-                }
+    let newWorker;
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/service-worker.js').then(reg => {
+            reg.addEventListener('updatefound', () => {
+                newWorker = reg.installing;
             });
         });
+    }
+    let refreshing;
+    navigator.serviceWorker.addEventListener('controllerchange', function () {
+        if (refreshing) return;
+        window.location.reload();
+        refreshing = true;
     });
-}
-let refreshing;
-navigator.serviceWorker.addEventListener('controllerchange', function () {
-    if (refreshing) return;
-    window.location.reload();
-    refreshing = true;
-});
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip({ trigger: "hover" })
-})
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip({ trigger: "hover" })
+    })
 
-particlesJS.load('particles-js', './assets/particles.json', function() {});
-particlesJS.load('particles-js2', './assets/particles.json', function() {});
+    particlesJS.load('particles-js', './assets/particles.json', function() {});
+    particlesJS.load('particles-js2', './assets/particles.json', function() {});
 
-VanillaTilt.init(document.querySelectorAll(".box"), {
-    max: 25,
-    speed: 400
-});
+    VanillaTilt.init(document.querySelectorAll(".box"), {
+        max: 25,
+        speed: 400
+    });
+}(jQuery);
