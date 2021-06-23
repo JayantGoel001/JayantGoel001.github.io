68be7b3bd01053908cbbcdf67a1594b58cce0ae1
Jayant Goel <jgoel92@gmail.com>
2021-06-24 02:51:30 +0530

    Initial Commit

--- service-worker.js
+++ service-worker.js
@@ -62,6 +62,7 @@ self.addEventListener("fetch", function (event) {
                                 caches.open(cacheName).then(function (cache) {
                                     if (event.request && requestClone.status!==206) {
                                         try {
+                                            console.log(requestClone.status);
                                             cache.put(event.request, responseClone);
                                         }catch (e){
                                             console.log(e);
