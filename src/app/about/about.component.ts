cc2121a46d566326afdf7e8251903266fc571dfd
JayantGoel001 <jgoel92@gmail.com>
2021-09-18 02:57:17 +0530

    Initial Commit

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+declare var data : any;
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+	  selector: 'app-about',
+	  templateUrl: './about.component.html',
+	  styleUrls: ['./about.component.css']
 })
+
 export class AboutComponent implements OnInit {
+	aboutData = data["About"];
+	constructor() { }
 
-  constructor() { }
+  	ngOnInit(): void {
 
-  ngOnInit(): void {
-  }
+  	}
 
 }
