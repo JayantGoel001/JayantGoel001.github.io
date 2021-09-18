074708d265b5a248f65a95a8af21f23c26eec6ec
JayantGoel001 <jgoel92@gmail.com>
2021-09-18 16:39:54 +0530

    Initial Commit

--- src/app/achievement/achievement.component.ts
+++ src/app/achievement/achievement.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var data : any
+
 @Component({
   selector: 'app-achievement',
   templateUrl: './achievement.component.html',
   styleUrls: ['./achievement.component.css']
 })
+
 export class AchievementComponent implements OnInit {
+	public achievementData  = data['Achievement']
+	constructor() { }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {}
 
 }
