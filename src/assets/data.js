a24eaab73a8c6035c19e2d2f94b5556274c71748
JayantGoel001 <jgoel92@gmail.com>
2021-09-18 16:10:15 +0530

    Initial Commit

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,12 +1,12 @@
 const identity = {
-	"name" : "Jayant Goel",
-	"image" : "assets/images/Profile-pic.webp",
+	name : "Jayant Goel",
+	image : "assets/images/Profile-pic.webp"
 }
 
 const data = {
-	"About" :{
+	About :{
 		...identity,
-		"NavTabs" : [
+		NavTabs : [
 			{
 				"id" : "story",
 				"name" : "About",
@@ -19,7 +19,7 @@ const data = {
 				"name" : "Education",
 			}
 		],
-		"about" : {
+		about : {
 			"tech-stats": "Full Stack Web Developer, Android, Data Science And DevOps Enthusiast",
 			"bio": "An Enthusiast Coder Eager to contribute to team success through hard work, skills, attention to details and excellent organizational skills. Currently, Exploring the world of Technology as Data Science Intern at Talent Decrypt and as a Final year student at Jaypee Institute Of Information Technology. \n Formerly worked as Android Developer At Theatron for 8 months.",
 			"IDE and Editors": [
@@ -39,9 +39,79 @@ const data = {
 				"Vagrant"
 			],
 		},
-		"skills":[
-			[],
-			[]
+		skills:[
+			[
+				{
+					skill : "Machine Learning",
+					progress : "90%"
+				},
+				{
+					skill : "Data Science",
+					progress : "90%"
+				},
+				{
+					skill : "Android",
+					progress : "90%"
+				},
+				{
+					skill : "Kotlin",
+					progress : "90%"
+				},
+				{
+					skill : "JAVA",
+					progress : "85%"
+				},
+				{
+					skill : "MEAN Stack Development",
+					progress : "80%"
+				}
+			],
+			[
+				{
+					skill : "C++",
+					progress : "90%"
+				},{
+					skill : "Python",
+					progress : "95%"
+				},{
+					skill : "HTML/CSS",
+					progress : "90%"
+				},{
+					skill : "Javascript",
+					progress : "85%"
+				},{
+					skill : "Git/GitHub",
+					progress : "90%"
+				},{
+					skill : "DevOps",
+					progress : "70%"
+				}]
+		],
+		education : [
+			{
+				animationClass : "bounceInRight left",
+				textAlignment : "right",
+				degree : "Bachelor of Technology (B.Tech), Computer Science And Technology",
+				college : "Jaypee Institute Of Information Technology, Noida Sector-62",
+				duration : "2018 - 2022",
+				percentage: "CGPA : 9.0"
+			},
+			{
+				animationClass : "bounceInLeft right",
+				textAlignment: "left",
+				degree : "XII (Senior Secondary), PCM with Computer Science",
+				college : "Holy Trinity Church School, Ghaziabad (ISC Board)",
+				duration : "Year of Completion : 2018",
+				percentage : "Percentage : 91%"
+			},
+			{
+				animationClass : "bounceInRight left",
+				textAlignment : "right",
+				degree : "X(Secondary)",
+				college : "Holy Trinity Church School, Ghaziabad (ICSE Board)",
+				duration : "Year of Completion : 2016",
+				percentage: "Percentage : 92.20%"
+			}
 		]
 	},
 
