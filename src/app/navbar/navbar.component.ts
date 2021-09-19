b35472fff318f0650aca141676141e5accb7133e
JayantGoel001 <jgoel92@gmail.com>
2021-09-19 14:35:38 +0530

    Initial Commit

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+declare var data : any;
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
+
 export class NavbarComponent implements OnInit {
+	public navbarData = data['NavBar'];
+	public activeClass = "Home";
+	constructor() {}
 
-  constructor() { }
+	ngOnInit(): void {
 
-  ngOnInit(): void {
-  }
+	}
 
 }
