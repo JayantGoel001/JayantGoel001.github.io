aaa63588dff13e4480115467819b504b3549a282
JayantGoel001 <jgoel92@gmail.com>
2021-09-19 13:45:19 +0530

    Initial Commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import Typed from 'typed.js';
+declare var data : any
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+	public homeData = data['Home'];
+	constructor() {}
 
-  ngOnInit(): void {}
-
+	ngOnInit(): void {
+		new Typed(".element",{strings: this.homeData['typedElement'], typeSpeed: 100, backDelay: 3000});
+	}
 }
