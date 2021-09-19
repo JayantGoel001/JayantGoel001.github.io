7134ddc1332389403dc20548735703705332f486
JayantGoel001 <jgoel92@gmail.com>
2021-09-19 14:10:38 +0530

    Initial Commit

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+declare var data : any;
 
 @Component({
-  selector: 'app-portfolio',
-  templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.css']
+	selector: 'app-portfolio',
+	templateUrl: './portfolio.component.html',
+	styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent implements OnInit {
+	public portfolioData = data['Portfolio'];
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {}
 
 }
