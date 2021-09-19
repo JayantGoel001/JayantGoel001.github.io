fdb28abbd66d2107f1c74e300e39636af6634f5c
JayantGoel001 <jgoel92@gmail.com>
2021-09-19 14:10:38 +0530

    Initial Commit

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var data : any;
+
 @Component({
-  selector: 'app-quote',
-  templateUrl: './quote.component.html',
-  styleUrls: ['./quote.component.css']
+	selector: 'app-quote',
+	templateUrl: './quote.component.html',
+	styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+	public quoteData = data['Quote'];
+	constructor() { }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {}
 
 }
