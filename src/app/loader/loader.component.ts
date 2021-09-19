58e20dd95baffad61f6e5ce526b3272a887a6ccb
JayantGoel001 <jgoel92@gmail.com>
2021-09-19 15:59:53 +0530

    Initial Commit

--- src/app/loader/loader.component.ts
+++ src/app/loader/loader.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoaderComponent implements OnInit {
 
-  constructor() { }
+	constructor() {
+	}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {}
 
 }
