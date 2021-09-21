f1c147087a24283eae0660df58ec6e51e4e996f6
JayantGoel001 <jgoel92@gmail.com>
2021-09-22 00:05:01 +0530

    Initial Commit

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,20 +3,6 @@
     $("#navbarCollapse").scrollspy({
         offset: 70
     });
-    let newWorker;
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/service-worker.js').then(reg => {
-            reg.addEventListener('updatefound', () => {
-                newWorker = reg.installing;
-            });
-        });
-    }
-    let refreshing;
-    navigator.serviceWorker.addEventListener('controllerchange', function () {
-        if (refreshing) return;
-        window.location.reload();
-        refreshing = true;
-    });
     $(function () {
         $('[data-toggle="tooltip"]').tooltip({ trigger: "hover" })
     })
