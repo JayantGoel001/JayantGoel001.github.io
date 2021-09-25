d3ffed9873a94588145a42be3ab9e1c942c6fe23
JayantGoel001 <jgoel92@gmail.com>
2021-09-25 17:58:35 +0530

    Initial Commit

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,8 +1,5 @@
 ! function($) {
     "use strict";
-    $("#navbarCollapse").scrollspy({
-        offset: 70
-    });
     $(function () {
         $('[data-toggle="tooltip"]').tooltip({ trigger: "hover" })
     })
