66c925011571f6d02a1cfa38ec3d5970fdd9d249
Jayant Goel <jgoel92@gmail.com>
2021-11-07 14:06:58 +0530

    Initial Commit

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,6 +11,14 @@ export class QuoteComponent implements OnInit {
 	public quoteData = data['Quote'];
 	constructor() { }
 
-	ngOnInit(): void {}
-
+	ngOnInit(): void {
+		fetch("https://api.quotable.io/random?minLength=25&&maxLength=120&&tags=technology|science|future").then(r => {
+			if(r.status === 200){
+				r.json().then(data =>{
+					this.quoteData['quote'] = data.content;
+					this.quoteData['author'] = data.author;
+				});
+			}
+		});
+	}
 }
