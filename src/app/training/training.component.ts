3963b7f4c2fab74d94c70650341fe63bf5284562
Jayant Goel <jgoel92@gmail.com>
2021-11-16 13:50:56 +0530

    Initial Commit

--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -11,6 +11,25 @@ export class TrainingComponent implements OnInit {
 	public trainingData = data['Training'];
 	constructor() {  }
 
-	ngOnInit(): void {  }
+	ngOnInit(): void {
+		const training = document.getElementById("training");
+		const flipBox = document.getElementsByClassName("services-flip-box");
+		const myTraining = document.getElementById("my-training");
+
+		if (training && myTraining) {
+			setInterval(() => {
+					if (myTraining.style.color == "white") {
+						myTraining.style.color = "black";
+					} else {
+						myTraining.style.color = "white";
+					}
+					for (let i = 0; i < flipBox.length; i++)
+						flipBox[i].classList.toggle("services-flip-box-dark-theme");
+
+					training.classList.toggle("section-codespaces-develop-night");
+				}
+				, 7100);
+		}
+	}
 
 }
