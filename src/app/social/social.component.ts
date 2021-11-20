d64fa3661eca668cc1ddf5791de85483c700ddf5
Jayant Goel <jgoel92@gmail.com>
2021-11-20 17:50:58 +0530

    Initial Commit

---
+++ src/app/social/social.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+declare var data : any;
+
+@Component({
+	selector: 'app-social',
+	templateUrl: './social.component.html',
+	styleUrls: ['./social.component.css']
+})
+
+export class SocialComponent implements OnInit {
+	public socialData = data['Social'];
+
+	constructor() {}
+
+	ngOnInit(): void {}
+
+}
