8285d05c0d6e843294ef78688fc5923bb9eca1c2
Jayant Goel <jgoel92@gmail.com>
2021-11-22 13:52:44 +0530

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,51 +4,6 @@ import {AppComponent} from "./app/app.component";
 
 const routes: Routes = [
 	{
-		path : 'A-Social-Media',
-		redirectTo : '/A-Social-Media'
-	}, {
-		path : 'Clockworks',
-		redirectTo : '/Clockworks'
-	}, {
-		path : 'Halloween',
-		redirectTo : '/Halloween'
-	},{
-		path : 'Smoke-Text',
-		redirectTo : "/Smoke-Text"
-	},{
-		path : 'Holy-Grail-Layout',
-		redirectTo : "/Holy-Grail-Layout"
-	}, {
-		path : 'Clock',
-		redirectTo : "/Clock"
-	}, {
-		path : 'Sleepy-Mondays',
-		redirectTo : "/Sleepy-Mondays"
-	}, {
-		path : 'Flexbox-Times',
-		redirectTo : "/Flexbox-Times"
-	}, {
-		path : 'Calendar',
-		redirectTo : "/Calendar"
-	}, {
-		path : 'Solar-System',
-		redirectTo : "/Solar-System"
-	}, {
-		path : 'Google',
-		redirectTo : "/Google"
-	}, {
-		path : 'Get-It-Done',
-		redirectTo : "/Get-It-Done"
-	}, {
-		path : 'Travel-Forest',
-		redirectTo : "/Travel-Forest"
-	}, {
-		path : 'Anime-List',
-		redirectTo : "/Anime-List"
-	}, {
-		path : 'awesome-Python-data-science-books',
-		redirectTo : "/awesome-Python-data-science-books"
-	}, {
 		path: '',
 		component : AppComponent,
 		pathMatch : 'full'
