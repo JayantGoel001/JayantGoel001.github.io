f227e254fa1a7567eb4330f68bce1c0e6edc7316
Jayant Goel <jgoel92@gmail.com>
2021-12-24 23:58:50 +0530

    Auto-generated commit [ci skip]

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.2';
+    const SW_VERSION = '13.0.3';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
