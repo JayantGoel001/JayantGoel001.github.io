6978f48a07c9dd52ce557edca4aa794ab6396d38
Jayant Goel <jgoel92@gmail.com>
2022-01-01 16:00:54 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,46 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {NgbTooltipModule} from "@ng-bootstrap/ng-bootstrap";
+import {ServiceWorkerModule} from '@angular/service-worker';
 
 import {AppComponent} from './app.component';
+import {ApplicationComponent} from './application/application.component';
+import {SplashComponent} from './splash/splash.component';
 import {HomeComponent} from './home/home.component';
+import {NavbarComponent} from './navbar/navbar.component';
 import {AboutComponent} from './about/about.component';
-import {PortfolioComponent} from './portfolio/portfolio.component';
 import {QuoteComponent} from './quote/quote.component';
+import {PortfolioComponent} from './portfolio/portfolio.component';
 import {TrainingComponent} from './training/training.component';
 import {AchievementComponent} from './achievement/achievement.component';
 import {ContactComponent} from './contact/contact.component';
+import {SocialComponent} from "./social/social.component";
 import {FooterComponent} from './footer/footer.component';
-import {NavbarComponent} from './navbar/navbar.component';
-import {SplashComponent} from './splash/splash.component';
+import {WaifuComponent} from './waifu/waifu.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+
 import {SplashScreenService} from "./splash-screen.service";
-import {ServiceWorkerModule} from '@angular/service-worker';
+
 import {environment} from '../environments/environment';
 import {AppRoutingModule} from "./app-routing.module";
-import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
-import {SocialComponent} from "./social/social.component";
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ApplicationComponent } from './application/application.component';
-import { WaifuComponent } from './waifu/waifu.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
+		ApplicationComponent,
+		SplashComponent,
 		HomeComponent,
+		NavbarComponent,
 		AboutComponent,
-		PortfolioComponent,
 		QuoteComponent,
+		PortfolioComponent,
 		TrainingComponent,
 		AchievementComponent,
 		ContactComponent,
-		FooterComponent,
-		NavbarComponent,
-		SplashComponent,
 		SocialComponent,
-		ApplicationComponent,
-		PageNotFoundComponent,
-  		WaifuComponent
+		FooterComponent,
+		WaifuComponent,
+		PageNotFoundComponent
 	],
 	imports: [
 		BrowserModule,
@@ -47,7 +49,7 @@ import { WaifuComponent } from './waifu/waifu.component';
 			enabled: environment.production,
 			registrationStrategy: 'registerImmediately'
 		}),
-		NgbModule
+		NgbTooltipModule
 	],
 	providers: [SplashScreenService],
 	bootstrap: [AppComponent]
