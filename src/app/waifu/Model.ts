626926d5700764972909d902a5e6d7da0ddb1c11
Jayant Goel <jgoel92@gmail.com>
2022-02-01 22:53:40 +0530

    Initial Commit
    Signed-off-by: Jayant Goel <jgoel92@gmail.com>

--- src/app/waifu/Model.ts
+++ src/app/waifu/Model.ts
@@ -1,22 +1,24 @@
 export class Model{
 
 	private baseURL : String = "assets/model/"
-	private readonly modelList : Array<Array<String>> = [[]];
+	private readonly modelList : any;
 	private readonly messages : Array<String> = [];
 
-	constructor(modelList : Array<Array<String>>,messages : Array<String>) {
+	constructor(modelList : any,messages : Array<String>) {
 		this.modelList = modelList;
 		this.messages = messages;
 	}
 
 	public get(modelID : number,textureID : number){
-		return `${this.baseURL}${this.modelList[modelID][textureID]}/index.json`;
+		let model = this.modelList['models'][modelID];
+		let textures = this.modelList['textures'][modelID][textureID];
+		return this.baseURL + model + '/' + (textures ? `${textures}/` : '') + 'index.json';
 	}
 	public switch(modelID : number){
-		modelID = (modelID + 1)%this.modelList.length;
+		modelID = (modelID + 1)%this.modelList['models'].length;
 		return {modelID : modelID,message : this.messages[modelID]};
 	}
 	public randTexture(modelID : number,textureID : number){
-		return (textureID + 1) % this.modelList[modelID].length;
+		return (textureID + 1) % this.modelList['textures'][modelID].length;
 	}
 }
