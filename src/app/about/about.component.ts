3f23bcfc194cc90cbdc2b6be0d9327bb0963f87e
JayantGoel001 <jgoel92@gmail.com>
2022-05-28 21:25:10 +0530

    Initial Commit
    Signed-off-by: Jayant Goel <jgoel92@gmail.com>

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
 
 declare var data : any;
 
@@ -9,15 +9,32 @@ declare var data : any;
 	styleUrls: ['./about.component.css']
 })
 
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit,AfterViewInit {
 	public aboutData = data["About"];
 	public activeTab = "story";
+	public selector : any;
 
 	constructor(public changeDetectorRef: ChangeDetectorRef) {
 		changeDetectorRef.detach();
 	}
 
 	ngOnInit(): void {
+		this.changeDetectorRef.detectChanges();
+	}
+	ngAfterViewInit() {
+		this.changeActiveTab(this.activeTab);
+	}
+
+	public changeActiveTab(id : string) {
+		const activeElement = document.getElementById(id+'-tab')!!;
+
+		if(!this.selector){
+			this.selector = document.getElementById('selector');
+		}
+		this.selector.style.width = `${activeElement.offsetWidth}px`;
+		this.selector.style.left = `${activeElement.offsetLeft}px`;
+
+		this.activeTab = id;
 		this.changeDetectorRef.detectChanges();
 	}
 }
